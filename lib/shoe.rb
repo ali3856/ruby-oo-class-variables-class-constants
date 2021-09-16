bda5b85b80e863e93324d20542fe6c52611794e3
Asim Ali <asimsali95@gmail.com>
2021-09-15 23:01:26 -0500

    commit tree 266794d2e081632cdf03e8921bf90239e657e4cc

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,14 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = [] #class contant 
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
@@ -11,4 +16,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  #BRANDS.uniq{|brand| puts @brand #confused on this line, think im headed in right direction
+
+  
 end
+
